6393d57707b510ae54b1e13b64e7392d4dbc1d62
StevenO <steven.odendaal@gallagher.com>
2022-12-13 14:05:32 +1300

    fixed safari issues

--- src/Components/WorkItem/WorkItem.jsx
+++ src/Components/WorkItem/WorkItem.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import './WorkItem.css'
-import ProjectData from '../../Data/ProjectData'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./WorkItem.css";
+import ProjectData from "../../Data/ProjectData";
+import { Link } from "react-router-dom";
 
 function WorkItem() {
   return (
-    <div className='workitems'>
-    {ProjectData.map((project, key) => {
-        return(
-        <Link key={key} className='workitem-container' to={"/my-work" + project.linkName} >
-            <div className='work-item-background'></div>
-            <div className='workitem-content'>
+    <div className="workitems">
+      {ProjectData.map((project, key) => {
+        return (
+          <Link
+            key={key}
+            className="workitem-container"
+            to={"/my-work" + project.linkName}
+          >
+            <div className="work-item-background"></div>
+            <div className="workitem-content">
               <h4> {project.projectName}</h4>
-                <p> {project.description}</p>
-              </div>
-            <img src={project.image} alt="project"/>
-        </Link>
-        )
+              <p> {project.description}</p>
+            </div>
+            <img className="project-image" src={project.image} alt="project" />
+          </Link>
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default WorkItem
+export default WorkItem;
