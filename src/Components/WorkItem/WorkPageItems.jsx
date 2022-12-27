b31fc7c44fb3bc031049abdc93bd0d2c5c6b90cc
StevenO <steven.odendaal@gallagher.com>
2022-12-27 17:32:12 +1300

    removed Link component

--- src/Components/WorkItem/WorkPageItems.jsx
+++ src/Components/WorkItem/WorkPageItems.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./WorkPageItems.css";
 import ProjectData from "../../Data/ProjectData";
-import { Link } from "react-router-dom";
 
 function WorkPageItems() {
   return (
